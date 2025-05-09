08c4341e977887576d535649f19ba02faf810568
alumno <alumno@PC-V202-24.uacj.mx>
2025-05-09 10:46:29 -0600

    [INICIO] Proyecto final

---
+++ proyecto/ProyectoFinal/ProyectoFinal/Coneccion_API/giphy_API.swift
@@ -0,0 +1,16 @@
+//
+//  giphy_API.swift
+//  ProyectoFinal
+//
+//  Created by alumno on 9/5/25.
+//
+
+import SwiftUI
+
+class giphyAPI: Codable{
+    let url_base = "https://opentdb.com/api.php?"
+    
+    func descargar_pagina_preguntas() async -> PaginaResultados?{
+        let ubicacion_recurso = ""
+    }
+}
---
+++ proyecto/ProyectoFinal/ProyectoFinal/ProyectoFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoFinalApp.swift
+//  ProyectoFinal
+//
+//  Created by alumno on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
